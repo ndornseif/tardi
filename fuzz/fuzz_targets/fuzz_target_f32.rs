67481a563c79f5653deeb8e728a7a13f7fa53b10
ndornseif <5382403+ndornseif@users.noreply.github.com>
2026-05-31 20:05:05 +0200

    Fuzz specifically the `f32` `MachineWord` version

--- fuzz/fuzz_targets/fuzz_target_f32.rs
+++ fuzz/fuzz_targets/fuzz_target_f32.rs
@@ -5,15 +5,16 @@ use libfuzzer_sys::arbitrary;
 use libfuzzer_sys::arbitrary::Arbitrary;
 
 use tardi::interpreter::{Interpreter};
+use tardi::consts::{MachineWord, Instruction};
 
 #[derive(Debug, Arbitrary)]
 struct Input {
-    bytecode: Vec<u8>,
-    data: Vec<f32>,
+    bytecode: Vec<Instruction>,
+    data: Vec<MachineWord>,
 }
 
-fuzz_target!(|data: Input| {
-    let mut interpreter = Interpreter::new_from_program(data.bytecode, data.data);
+fuzz_target!(|input: Input| {
+    let mut interpreter = Interpreter::new_from_program(input.bytecode, input.data);
     interpreter.execute();
 });
 
